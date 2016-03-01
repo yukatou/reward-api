9d4c51f605e9837faa2d4453b902fba9169c74b0
yukatou <yu.lets0226@gmail.com>
2016-03-01 13:44:43 +0900

    update files.

--- adstir.php
+++ adstir.php
@@ -1,18 +1,29 @@
 <?php
 
-// キーとする項目以外はデフォルト空とする
+/**
+ * NOTE: 
+ * Adstirは uidとtransaction_idで重複判定を行うこと
+ * テーブルは２つ用意する （ユーザからのお問い合わせに対応するため）
+ * ①  情報管理用テーブル t_adstir 付与した時のみinsert
+ * ②  ポイントログテーブル t_adstir アクセスログとして使用
+ *
+ * レスポンスに関して、Adstirは特に指定がないのでAppDriverと同じようにしておく
+ * ポイント付与成功時はレスポンスボディに1を返す
+ * 0は成功だがポイント未付与
+ * それ以外はエラーとなる
+ */
+
 $type				=	Input::get('type');		// １日１回: top ビューア: read
 $uid        = Input::get('uid') //ユーザの紹介コードが送られてくる
 $transaction_id	=	Input::get('transaction_id');
 $currency		=	Input::get('currency'); // 特に使わない
-$amount			=	Input::get('amount'); // ハート数
+$amount			=	Input::get('amount'); // 付与するハート数
 $dateval		=	date("c");
 
 $user_id;
 
-
-// IPの制限
-if(validate_remote_address() == false){
+// アクセス元のIP制御
+if (validate_remote_address() == false) {
   Log::info('invalid_IP:'.json_encode(Input::real_ip()));
   Response::forge('', 500)->send(true);
   exit;
@@ -20,83 +31,135 @@ if(validate_remote_address() == false){
 
 // ユーザ認証
 $ret = validate_user($uid, $user_id);
-if($ret != true){
+if ($ret != true) {
   Response::forge('', 400)->send(true);
   exit;
 }
 
-$count	= getRecCount('t_adstir', $user_id, $transactions_id, 1);
-
-//--------------------------------------
-// アイテム付与済、CARward指定のOKを返却
-//--------------------------------------
-if($count > 0){
-  //--------------------------------------
-  //	 正常終了（ポイント付与済）
-  //--------------------------------------
-  echo "OK";
-  exit;
-}
-
-//
-// １日１回動画であれば、その日に動画をみている場合はスキップさせる
-//
-if ($type === "top") {
-
-  // 
-  // もう動画をみているか確認する処理
-  //
-
-  if ("見ていた場合") {
-    // 何もしないで正常終了させる
-    echo "OK";
-    exit;
-  }
-}
+$count_adstir	= $this->_getRecCount('t_adstir',$uid, $transaction_id);
+$count_adstir_log	= $this->_getRecCount('t_adstir_log',$uid, $transaction_id); 
 
 try {
 
   DB::start_transaction();
-
+  //--------------------------------------
+  //①	t_adstir_log insert
+  //	ログは毎回登録
+  //--------------------------------------
   $sql = <<<_SQL
-    INSERT INTO t_adstir (
-      user_id
-      ,uid
+    INSERT INTO t_adstir_log (
+      uid
       ,type
       ,transaction_id
-      ,amount
       ,currency
+      ,amount
       ,created
     )
     VALUES	 (
-      :user_id
-      ,:uid
+      :uid
       ,:type
       ,:transaction_id
-      ,:amount
       ,:currency
+      ,:amount
       ,:created
     )
     _SQL
     ;
 
   $q = DB::query($sql)
-    ->bind('user_id', $user_id)
-    ->bind('uid', $uid)
-    ->bind('type', $type)
     ->bind('transaction_id', $transaction_id)
-    ->bind('amount', $amount)
+    ->bind('type', $type)
     ->bind('currency', $currency)
+    ->bind('amount', $amount)
     ->bind('created', $dateval)
     ->execute()
     ;
 
-  //--------------------------------------
-  // アイテム付与（回復BOX赤固定）
-  //--------------------------------------
-  $ret = get_item($user_id,$point);
 
-  if($ret != true) {
+  //--------------------------------------
+  // アイテム付与済の場合は0を返却
+  //--------------------------------------
+  if($count_adstir > 0){
+    if($count_adstir_log != 0){
+      //ログのみコミット
+      DB::commit_transaction();
+      //--------------------------------------
+      //	 正常終了（ポイント付与済）
+      //--------------------------------------			
+      echo 0;
+      exit();
+    } else {
+      //このルートの場合は、t_adstirにレコードがあるが、ステータスが未完了で終了した場合
+      //従って、処理は続行させる。
+    }
+  }
+
+  //--------------------------------------
+  // １日１回動画であれば、その日に動画をみている場合はスキップさせる
+  //--------------------------------------
+  if ($type === "top") {
+
+    // 
+    // もう動画をみているか確認する処理
+    //
+
+    if ("見ていた場合") {
+      // 何もしないで正常終了させる
+      echo 0;
+      exit;
+    }
+  }
+
+
+  //--------------------------------------
+  // t_adstir insert
+  //--------------------------------------
+  $sql = <<<_SQL
+    INSERT INTO t_adstir (
+      uid
+      ,type
+      ,transaction_id
+      ,amount
+      ,created
+    )
+    VALUES	 (
+      :uid
+      ,:type
+      ,:transaction_id
+      ,:amount
+      ,:created
+    )
+    _SQL
+    ;
+  try {
+
+    $q = DB::query($sql)
+      ->bind('uid', $uid)
+      ->bind('transaction_id', $transaction_id)
+      ->bind('amount', $amount)
+      ->bind('created', $dateval)
+      ->execute()
+      ;
+
+  } catch (Exception $e) {
+    // Log::info(json_encode(DB::error_info()));
+    $errorinfo = DB::error_info();
+    if($errorinfo[1] == 1062){
+      //重複はスルー
+    } else {
+      //その他例外
+      DB::rollback_transaction();
+      Response::forge('', 400)->send(true);
+      exit;
+    }
+  }
+
+  //--------------------------------------
+  //④ アイテム付与
+  //--------------------------------------
+  $ret = get_item($user_id,$amount);
+
+  if($ret != true){
     DB::rollback_transaction();
     Response::forge('', 400)->send(true);
     exit;
@@ -110,17 +173,6 @@ try {
     // app_usersのwatch_ad_dateに日付をいれる処理
     //
   }
-
-  //--------------------------------------
-  // ステータス更新(t_adsir)
-  //--------------------------------------
-  $adstir = Model_Adstir::query()
-    ->where('user_id', $user_id)
-    ->where('transaction_id', $transaction_id)
-    ->get_one();
-  $adstir->status = 1;
-  $adstir->modified = date('c');
-  $adstir->save();
 
   DB::commit_transaction();
 
@@ -136,8 +188,8 @@ try {
 
 //--------------------------------------
 //	 正常終了（ポイント付与完了）
-//--------------------------------------
-echo "OK";
+//--------------------------------------			
+echo 1;
 
 /**
  * ユーザ認証を行う
@@ -147,41 +199,37 @@ function authUser($uuid) {
 
   if(!$uuid) {
     return false;
-  }
-  $user = Model_Client::query()->where('introduce_code', $uuid)->get_one();
+  }		
+  $user = Model_Client::query()->where('introduce_cd', $uuid)->get_one();
   if(!$user) {
     return false;
-  }
+  }		
 
   return $user->id;
 }
-
 
 /**
  * ユーザ認証を行う
  * @return boolean|string
  */
-function validate_user($introduction_code, &$user_id) {
-  $user_id = authUser($introduction_code);
+function validate_user($identifier,&$user_id) {
+  $user_id = authUser($identifier);
   if(!$user_id) {
     return false;
   }
 
   $user = Model_Client::find($user_id);
-
   if(!$user) {
     return false;
   }
-
   return true;
 }
-
 
 /**
  * ITEM購入
  * @return boolean
  */
-function get_item($user_id, $reward_point = 0) { 
+function get_item($user_id,$reward_point = 0) {
   //
   // $rewart_point分の回復アイテムを付与する処理
   //
@@ -192,17 +240,12 @@ function get_item($user_id, $reward_point = 0) {
  * レコード件数の確認
  * @return レコード数
  */
-function getRecCount($tablename, $key1, $key2, $key3, $key4, $status) {
+function getRecCount($tablename,$uid,$transaction_id) {
 
   $q = null;
   $q = \DB::select(\DB::expr('COUNT(*) as cnt'))->from($tablename);
-  $q->where('uid', '=', $key1);
-  $q->where('cid', '=', $key2);
-  $q->where('action_date', '=', $key3);
-  $q->where('pid', '=', $key4);
-  if($status == true){
-    $q->where('status', '=', $status);
-  }
+  $q->where('uid', '=', $uid);
+  $q->where('transaction_id', '=', $transaction_id);
 
   $ret = $q->execute()->current();
 
@@ -221,5 +264,9 @@ function validate_remote_address($whitelist=null) {
     );
   }
 
-  return true;
+  if(in_array($ip, $whitelist)) {
+    return true;
+  }
+
+  return false;
 }
